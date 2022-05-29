28514b25a0fb22699c3dd528e3a54e1f43a4207d
LorenaMendes <lorenamendes.contato@gmail.com>
2022-05-29 12:24:36 -0400

    add function to load player details

--- public/js/index.js
+++ public/js/index.js
@@ -10,14 +10,16 @@ async function loadGames() {
     for (let g of Object.keys(games)) {
         var cur_game = games[g];
         
-        ul_content += ` <div class="text-and-icons">
-                            <li><a onclick="loadGame('${cur_game.id}')">
-                            ${cur_game.nick}
-                            </a></li>
-                            <a title="Remove game" onclick="removeGame('${cur_game.id}')">
-                            <i class="fa fa-ban icon"></i>
-                            </a>
-                        </div>`
+        ul_content += `
+            <div class="text-and-icons">
+                <li class="clickable-text"><a onclick="loadGame('${cur_game.id}')">
+                ${cur_game.nick}
+                </a></li>
+                <a title="Remove game" onclick="removeGame('${cur_game.id}')">
+                <i class="fa fa-ban icon"></i>
+                </a>
+            </div>
+        `
     }
 
     var html = `
@@ -38,43 +40,113 @@ async function loadGame(gid) {
     var players = '';
     for (let p of Object.keys(game.players)) {
         var player = game.players[p];
-        players += `<div class="text-and-icons"><li>${player.nick}</li> <a title="Kick player" onclick="removePlayer('${gid}', '${p}')"><i class="fa fa-ban icon"></i></a></div>`
+        players += `
+            <div class="text-and-icons">
+                <a onclick="loadPlayerDetails('${gid}', '${p}')">
+                    <li class="clickable-text">${player.nick}  -  ${player.score} points</li>
+                </a>
+                <a title="Kick player" onclick="removePlayer('${gid}', '${p}')">
+                    <i class="fa fa-ban icon"></i>
+                </a>
+            </div>
+        `
     }
 
-    var html = `<div class="game-data-title">Game: ${game.nick}</div>
-                <br><br>
-                <div class="text-and-icons">
-                    <span>DECK INFO</span>
-                    <a title="Shuffle deck"><i class="fa fa-random icon"></i></a>
-                    <a title="Add deck" onclick="addDeck('${gid}')"><i class="fa fa-plus icon"></i></a>
-                </div>
-                <div class="deck-info">
-                    <span>Total: ${game.deck.length} cards</span>
-                    <ul>
-                        <li>24 hearts</li>
-                        <li>26 spades</li>
-                        <li>25 clubs</li>
-                        <li>21 diamonds</li>
-                    </ul>
-                </div>
-                
-                <div>
-                        <span>PLAYERS INFO</span><br>
-                    </div>
-                    <div class="deck-info">
-                        <ul>
-                            ${players}
-                        </ul>
-                    </div>
-                    
-                    <div class="add-player">
-                        <input id="playerNick" type="text" class="form-control input-add" placeholder="new player"/>
-                        <a title="Add player" onclick="addPlayer('${gid}')"><i class="fa fa-plus icon"></i></a>
-                    </div>
-                `
+    var html = `
+        <div class="game-data-title">Game: ${game.nick}</div>
+        <br><br>
+        <div class="text-and-icons">
+            <span>DECK INFO</span>
+            <a title="Shuffle deck"><i class="fa fa-random icon"></i></a>
+            <a title="Add deck" onclick="addDeck('${gid}')"><i class="fa fa-plus icon"></i></a>
+        </div>
+        <div class="deck-info">
+            <span>Decks in this game: ${game.decks} (${game.decks * 52} cards)</span>
+            <span>Undealt cards: ${game.undealt.length} cards</span>
+            <span>Dead cards: ${game.deadCards} cards</span>
+            <ul>
+                <li>24 hearts</li>
+                <li>26 spades</li>
+                <li>25 clubs</li>
+                <li>21 diamonds</li>
+            </ul>
+        </div>
+        
+        <div>
+            <span>PLAYERS INFO</span><br>
+        </div>
+        <div class="add-player">
+            <input id="playerNick" type="text" class="form-control input-add" placeholder="new player"/>
+            <a title="Add player" onclick="addPlayer('${gid}')"><i class="fa fa-plus icon"></i></a>
+        </div>
+        <div class="deck-info">
+            <ul>
+                ${players}
+            </ul>
+        </div>
+        
+    `
 
     document.getElementById('game-data').innerHTML = html;
 }
+
+async function loadPlayerDetails(gid, pid) {
+
+    let response = await fetch(`/api/get/player?gid=${gid}&pid=${pid}`);
+    var player = await response.json();
+    
+    player = player['player'];
+
+    var player_cards = '';
+    for (var card of player.cards) {
+        let symbol, color;
+        switch (card.suit) {
+            case 'hearts':
+                symbol = '♥';
+                color = 'red';
+                break;
+            case 'clubs':
+                symbol = '♣';
+                color = 'black';
+                break;
+            case 'diamonds':
+                symbol = '♦';
+                color = 'red';
+                break;
+            case 'spades':
+                symbol = '♠';
+                color = 'black';
+                break;
+        }
+        
+        player_cards += `
+            <div class="card ${color}-card">
+                <div class="card-face">${card.face}</div>
+                <div class="card-symbol">
+                    ${symbol}
+                </div>
+            </div>
+        `;
+    }
+
+    var html = `
+        <div class="game-data-title">Player: ${player.nick} - ${player.score} points</div>
+        <br><br>
+        <div class="text-and-icons">
+        </div>
+        <div class="player-cards-container">
+            ${player_cards}
+        </div>
+        <div class="deal-cards-menu">
+            <input id="numberOfCards" type="number" class="form-control input-add" value="1" min="1" placeholder="1"/>
+            <a title="Deal cards" onclick="dealCards('${gid}', '${pid}')"><i class="fa fa-plus icon"></i></a>
+        </div>
+    `;
+        
+    document.getElementById('data-details').innerHTML = html;
+}
+
+async function loadDeckDetails(gid) {}
 
 async function addGame(nick) {
     console.log('ADD GAME');
@@ -97,7 +169,6 @@ async function removeGame(gid) {
     console.log(result);
 
     document.getElementById('game-data').innerHTML = '';
-
     loadGames();
 }
 
@@ -129,7 +200,18 @@ async function removePlayer(gid, pid) {
 
     let response = await fetch(`/api/remove/player?gid=${gid}&pid=${pid}`);
     var result = await response.json();
-    console.log(result);
+
+    document.getElementById('data-details').innerHTML = '';
+    loadGame(gid);
+}
+
+async function dealCards(gid, pid) {
+    var qt = document.getElementById('numberOfCards').value;
+    if(qt <= '0' || qt == undefined) qt = '1';
+
+    let response = await fetch(`/api/add/player_cards?gid=${gid}&pid=${pid}&qt=${qt}`);
+    var result = await response.json();
 
     loadGame(gid);
+    loadPlayerDetails(gid, pid);
 }
